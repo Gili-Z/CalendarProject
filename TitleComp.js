9d69843c9c103cd99dfc518c2a1d697e0c4fc01f
Gili-Z <gili4prez@gmail.com>
2021-07-09 14:31:57 -0700

    Adding sheet functionality

--- TitleComp.js
+++ TitleComp.js
@@ -1,6 +1,27 @@
+//test function is unpredictable! understand its counterintuitive behavior
+
 function titleComp() {
+
+  let sheet = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/18za7Rgyy9j5dho4KFa7zv6vnxJh4T9wa7sSVL8RMJb0/edit?usp=sharing');
+  SpreadsheetApp.setActiveSpreadsheet(sheet);
+  SpreadsheetApp.setActiveSheet(sheet.getSheets()[0]);
+ 
+  let range = SpreadsheetApp.getActiveSheet().getRange('A2:B');
+  SpreadsheetApp.setActiveRange(range);
+  let formats = [];
+
+  for (i = 1; i<1000000; i++){
+    let cell = range.getCell(i, 1);
+    let flag = range.getCell(i, 2);
+    if (cell.isBlank()){
+      break;
+    } else{
+      // auto global right now - update so it gets flag too?
+      formats.push(new RegExp(cell.getValue(), flag.getValue()));
+    }
+  }
+
   let cals = CalendarApp.getAllCalendars();
-  let formats = [/[1-9]\w+/] ///\b[A-Z][a-z]+\b/
 
   for (i in cals){
     let events = cals[i].getEvents(new Date(), new Date((new Date()).getTime()+7*24*60*60*1000));
@@ -8,9 +29,9 @@ function titleComp() {
       let title = events[j].getTitle();
       let creator = String(events[j].getCreators());
       let should_i_email = true;
+      Logger.log(title);
 
       for (k in formats){
-        Logger.log(title);
         Logger.log(formats[k].test(title));
         if (formats[k].test(title) == true){
           should_i_email = false;
@@ -20,7 +41,7 @@ function titleComp() {
 
       if (should_i_email == true) {
         // this would email 'creator', but i don't want to accidentally email anyone right now
-        GmailApp.sendEmail('gili4prez@gmail.com', "(BOT) Calendar Naming Issue", `Hello!\n\nYour event: "${title}" at ${events[j].getStartTime()} was titled incorrectly. Please refer to the spreadsheet at this <link> to review naming conventions.\n\nThank you!\nFrom Calbot`); // set up spreadsheet with reg expressions, example names in column next over for reference and naming updates
+        GmailApp.sendEmail('gili4prez@gmail.com', "(BOT) Calendar Naming Issue", `Hello!\n\nYour event: "${title}" at ${events[j].getStartTime()} was titled incorrectly. Please refer to the spreadsheet at https://docs.google.com/spreadsheets/d/18za7Rgyy9j5dho4KFa7zv6vnxJh4T9wa7sSVL8RMJb0/edit?usp=sharing to review naming conventions.\n\nThank you!\nFrom Calbot`);
       }
     }
   }
