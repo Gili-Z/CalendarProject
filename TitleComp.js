dc8cbe214a148570f8e2ef52024ca178d637e3c2
Gili-Z <gili4prez@gmail.com>
2021-07-15 13:59:52 -0700

    Adding a draft of the time compiling function

--- TitleComp.js
+++ TitleComp.js
@@ -20,8 +20,11 @@ function titleComp(start = new Date(), end = new Date((new Date()).getTime()+7*2
     }
   }
 
-  // getting calendar events
+  // getting calendar events + data
   let cals = CalendarApp.getAllCalendars();
+  let types = [];
+  let users = [];
+  let nested = [];
 
   for (i in cals){
     let events = cals[i].getEvents(new Date(start), new Date(end));
@@ -31,15 +34,34 @@ function titleComp(start = new Date(), end = new Date((new Date()).getTime()+7*2
       let should_i_email = true;
       // Logger.log(title);
 
-      // checking if title is a match with any format
+      // checking if title is a match with any format; if so, adds its data to the data arrays
       for (k in formats){
         formats[k].lastIndex = 0;
         match = formats[k].test(title);
         // Logger.log(match);
         if (match){
           should_i_email = false;
+          let event_type = title.match(formats[k])[1];
+          let duration = (events[j].getEndTime()-events[j].getStartTime())/3600000;
+          
+          if (!types.includes(event_type)){
+            types.push(event_type);
+            nested.push([]);
+          }
+
+          if (!users.includes(creator)){
+            users.push(creator);
+          }
+
+          if(nested[types.indexOf(event_type)][users.indexOf(creator)]==null){
+            nested[types.indexOf(event_type)][users.indexOf(creator)] = (duration);
+          } else{
+            nested[types.indexOf(event_type)][users.indexOf(creator)] += (duration);
+          }
         }
+        if (should_i_email == false){break};
       }
+  
       // Logger.log(`Format loop completed for ${events[j].getTitle()}. Should I email? ${should_i_email}`);
 
       // sending email if title does not match any formats
@@ -49,4 +71,43 @@ function titleComp(start = new Date(), end = new Date((new Date()).getTime()+7*2
       }
     }
   }
+
+  // adding data to data sheet
+  sheet = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1PGYLqkRZyxtXB3rmpxTpTuCZz74BV4VQKf71fW0rWVI/edit?usp=sharing');
+  SpreadsheetApp.setActiveSpreadsheet(sheet);
+  SpreadsheetApp.setActiveSheet(sheet.getSheets()[0]);
+  
+  let lower_corner = `${String.fromCharCode(98 + users.length).toUpperCase()}${2+types.length}`;
+
+  // clear sheet
+  range = SpreadsheetApp.getActiveSheet().getRange(`B2:Z`);
+  range.clear();
+  
+  // set new range and enter data
+  range = SpreadsheetApp.getActiveSheet().getRange(`B2:${lower_corner}`);
+  SpreadsheetApp.setActiveRange(range);
+  
+  // setting event and user names
+  for (i in types){
+    range.getCell(parseInt(i)+2, 1).setValue(types[i]);
+  }
+
+  for (i in users){
+    range.getCell(1, parseInt(i)+2).setValue(users[i]);
+  }
+
+  // entering time in hours
+  for (i in types){
+    for (j in users){
+      range.getCell(parseInt(i)+2, parseInt(j)+2).setValue(nested[i][j]);
+    }
+  }
 }
+
+
+// add total bar at end of each row (and column)
+
+
+
+
+
